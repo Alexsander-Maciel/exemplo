ad8d644dc1baf55a5e844c4f271026d6a9d5b239
alexmaciel <alexmaciel@alexmaciel-Inspiron>
2019-01-29 13:49:00 -0200

    Alteração do arquivo postagens

--- application/views/postagens.php
+++ application/views/postagens.php
@@ -5,11 +5,16 @@
         <title>Meu Blog</title>
     </head>
     <body>
+        <?php 
+        echo anchor(base_url(),"Home") .
+        anchor(base_url("fale-conosco"), "Fale Conosco");
+        ?>
         <h3>Meu Blog</h3>
+        <h3>Postagens Recentes</h3>
         <?php
         foreach ($postagens as $post)
         {
-            $lista_urls[] = anchor(base_url("detalhes/".$post->id),$post->titulo);
+            $lista_urls[] = anchor(base_url("/welcome/detalhes/".$post->id),$post->titulo);
         }
         echo ul($lista_urls);
         ?>
