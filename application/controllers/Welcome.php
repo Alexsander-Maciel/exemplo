d939e747ce0a030f7d643b2bd2ae4ae7c299a232
alexmaciel <alexmaciel@alexmaciel-Inspiron>
2019-01-30 14:08:32 -0200

    Inserido o método do formulario de contato

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -36,4 +36,38 @@ class Welcome extends CI_Controller {
             $data['postagens'] = $this->db->get('postagens')->result();
             $this->load->view('detalhes',$data);
         }
+        
+        public function fale_conosco()
+        {
+            $this->load->helper('form');
+            $this->load->view('fale_conosco');
+        }
+        
+        public function enviar_mensagem()
+        {
+            $mensagem = "Nome:".$this->input->post('txt_nome') . br();
+            $mensagem = "E-mail:".$this->input->post('txt_email') . br();
+            $mensagem = "Mensagem:".$this->input->post('txt_mensagem') . br();
+            $config['protocol'] = 'smtp';
+            $config['smtp_host'] = 'ssl://smtp.googlemail.com';
+            $config['smtp_port'] = '465';
+            $config['smtp_timeout'] = '30';
+            $config['smtp_user'] = 'seu email@gmail.com';
+            $config['smtp_pass'] = 'sua senha';
+            $config['charset'] = 'utf-8';
+            $config['newline'] = '\r\n';
+            $config['mailtype'] = 'html';
+            $this->load->library('email',$config);
+            $this->email->from("seu email@gmail.com", "Formulario do site");
+            $this->email->to("email que recebe@gmail.com");
+            $this->email->subject('Assunto do Email');
+            $this->email->message($mensagem);
+            if($this->email->send())
+            {
+                $this->load->view('sucesso_envia_contato');
+            }else{
+                print_r($this->email->print_debugger());
+            }
+            
+        }
 }
