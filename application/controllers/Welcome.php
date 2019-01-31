5d8e6fd51cdf1d1cc6adbcdf10f60d6fce1ee148
alexmaciel <alexmaciel@alexmaciel-Inspiron>
2019-01-31 15:00:49 -0200

    Alterado o método da fuunção enviar_mensagem

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,53 +1,54 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
-        
-        public function teste()
-        {
-            $data['postagens'] = $this->db->get('postagens')->result();
-            $this->load->view('postagens',$data);
-        }
-        
-        public function detalhes($id)
-        {
-            $this->db->where('id',$id);
-            $data['postagem'] = $this->db->get('postagens')->result();
-            $data['postagens'] = $this->db->get('postagens')->result();
-            $this->load->view('detalhes',$data);
-        }
-        
-        public function fale_conosco()
-        {
-            $this->load->helper('form');
-            $this->load->view('fale_conosco');
-        }
-        
-        public function enviar_mensagem()
-        {
-            $mensagem = "Nome:".$this->input->post('txt_nome') . br();
-            $mensagem = "E-mail:".$this->input->post('txt_email') . br();
-            $mensagem = "Mensagem:".$this->input->post('txt_mensagem') . br();
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+        $this->load->view('welcome_message');
+    }
+
+    public function teste() {
+        $data['postagens'] = $this->db->get('postagens')->result();
+        $this->load->view('postagens', $data);
+    }
+
+    public function detalhes($id) {
+        $this->db->where('id', $id);
+        $data['postagem'] = $this->db->get('postagens')->result();
+        $data['postagens'] = $this->db->get('postagens')->result();
+        $this->load->view('detalhes', $data);
+    }
+
+    public function fale_conosco() {
+        $this->load->helper('form');
+        $this->load->view('fale_conosco');
+    }
+
+    public function enviar_mensagem() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('txt_nome', 'Email', 'required');
+        $this->form_validation->set_rules('txt_email', 'Email', 'required/valid_email');
+        $this->form_validation->set_rules('txt_mensagem', 'Email', 'required');
+        if ($this->form_validation->run()) {
+            $mensagem = "Nome:" . $this->input->post('txt_nome') . br();
+            $mensagem = "E-mail:" . $this->input->post('txt_email') . br();
+            $mensagem = "Mensagem:" . $this->input->post('txt_mensagem') . br();
             $config['protocol'] = 'smtp';
             $config['smtp_host'] = 'ssl://smtp.googlemail.com';
             $config['smtp_port'] = '465';
@@ -57,17 +58,18 @@ class Welcome extends CI_Controller {
             $config['charset'] = 'utf-8';
             $config['newline'] = '\r\n';
             $config['mailtype'] = 'html';
-            $this->load->library('email',$config);
+            $this->load->library('email', $config);
             $this->email->from("seu email@gmail.com", "Formulario do site");
             $this->email->to("email que recebe@gmail.com");
             $this->email->subject('Assunto do Email');
             $this->email->message($mensagem);
-            if($this->email->send())
-            {
+            if ($this->email->send()) {
                 $this->load->view('sucesso_envia_contato');
-            }else{
+            } else {
                 print_r($this->email->print_debugger());
             }
-            
+        } else {
+            $this->fale_conosco();
         }
+    }
 }
