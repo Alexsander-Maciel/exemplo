4a5a49fcc21231577c19a6bcd2a42caf75983265
alexmaciel <alexmaciel@alexmaciel-Inspiron>
2019-01-31 15:04:55 -0200

    incluido a validação dos campos

--- application/views/fale_conosco.php
+++ application/views/fale_conosco.php
@@ -11,20 +11,20 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo anchor(base_url(),"Home").
-        anchor(base_url("fale-conosco","Fale Conosco")).
-        heading("Meu Blog",2).
-        heading("Fale Conosco",3);
-        $atributos = array('name' => 'formulario_contato','id' => 'formulario_contato');
-        echo form_open(base_url('welcome/enviar_mensagem'),$atributos).
-        form_label("Nome:","txt_nome") . br().
-        form_input('txt_nome') . br().
-                form_label("Email:","txt_email") . br().
-        form_input('txt_email') . br().
-                form_label("Mensagem:","txt_mensagem") . br().
-                form_textarea('txt_mensagem') . br().
-                form_submit("btn_enviar","Enviar Mensagem").
-                form_close();
+        echo anchor(base_url(), "Home") .
+        anchor(base_url("fale-conosco", "Fale Conosco")) .
+        heading("Meu Blog", 2) .
+        heading("Fale Conosco", 3);
+        $atributos = array('name' => 'formulario_contato', 'id' => 'formulario_contato');
+        echo form_open(base_url('welcome/enviar_mensagem'), $atributos) .
+        form_label("Nome:", "txt_nome") . br() .
+        form_input('txt_nome', set_value('txt_nome')) . br() .
+        form_label("Email:", "txt_email") . br() .
+        form_input('txt_email',set_value('txt_email')) . br() .
+        form_label("Mensagem:", "txt_mensagem") . br() .
+        form_textarea('txt_mensagem',set_value('txt_mensagem')) . br() .
+        form_submit("btn_enviar", "Enviar Mensagem") .
+        form_close();
         ?>
     </body>
 </html>
